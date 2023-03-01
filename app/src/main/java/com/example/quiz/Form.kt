b16aa8d1b57e3cbdf8ff35a7dde1a8f8f2496122
fanta <fanta@DESKTOP-G89TUP9>
2023-03-01 13:33:39 +0100

    Adding JSON API

--- app/src/main/java/com/example/quiz/Form.kt
+++ app/src/main/java/com/example/quiz/Form.kt
@@ -1,11 +1,12 @@
 package com.example.quiz
+import com.google.gson.Gson
 
 class Form {
 
-    var id: Int
-    var nombre: String
-    var tema: String
-    var preguntas: List<Question>
+    var id: Int = 0
+    var nombre: String = ""
+    var tema: String = ""
+    var preguntas: List<Question> = emptyList()
 
     constructor(id: Int, tema: String, nombre: String, preguntas: List<Question>) {
         this.id = id
@@ -14,4 +15,11 @@ class Form {
         this.preguntas = preguntas
     }
 
+    constructor()
+
+    companion object {
+        fun fromJson(json: String): Form {
+            return Gson().fromJson(json, Form::class.java)
+        }
+    }
 }
--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -9,14 +9,26 @@ import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Button
 import android.widget.Toast
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 
 class MainActivity : AppCompatActivity() {
+
+    private var formulario: Form? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario)
 
         val formulario = getFormulario() // Este método obtiene un objeto Formulario
 
+        /*
         val layoutPreguntas = findViewById<LinearLayout>(R.id.layout_preguntas)
         val radioGroups = mutableListOf<RadioGroup>()
         for (pregunta in formulario.preguntas) {
@@ -55,16 +67,66 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
         }
         layoutPreguntas.addView(botonValidar)
+
+         */
     }
 
-    private fun getFormulario(): Form {
+    fun jsonToForm(json: String): Form {
+        val gson = Gson()
+        return gson.fromJson(json, Form::class.java)
+    }
+
+    private fun getFormulario() {
+        /*
         val preguntas = mutableListOf(
             Question(1, "¿Cuál es tu color favorito?","random", listOf("Rojo", "Azul", "Verde", "Amarillo"), 1),
             Question(2, "¿Cuál es tu comida favorita?","random",  listOf("Pizza", "Hamburguesa", "Tacos", "Sushi"), 0),
             Question(3, "¿Cuál es tu deporte favorito?","random",  listOf("Fútbol", "Béisbol", "Baloncesto", "Tenis"), 2)
         )
         return Form(1, "random", "Formulario de ejemplo", preguntas)
+
+         */
+        val url = "https://opoblueblood.000webhostapp.com/"
+        getJsonFromUrlAsync(url, object : JsonCallback {
+            override fun onJsonReceived(json: String) {
+                println(json)
+                // Aquí puedes procesar el JSON recibido
+                val form = jsonToForm(json)
+                val pregunta = form.preguntas[0].texto
+                println(pregunta)
+
+            }
+
+            override fun onError(exception: Exception) {
+                println("Error al obtener el JSON: ${exception.message}")
+            }
+        })
+
     }
+
+
+    interface JsonCallback {
+        fun onJsonReceived(json: String)
+        fun onError(exception: Exception)
+    }
+
+    fun getJsonFromUrlAsync(url: String, callback: JsonCallback) {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                callback.onError(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val json = response.body?.string() ?: ""
+                callback.onJsonReceived(json)
+            }
+        })
+    }
+
 
 
 
