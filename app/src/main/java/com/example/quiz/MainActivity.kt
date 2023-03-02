31da15bc2d6180fe13cb361276ad863d53868156
fanta <fanta@DESKTOP-G89TUP9>
2023-03-02 11:25:21 +0100

    Adding JSON API Functional

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -20,55 +20,10 @@ import com.google.gson.annotations.SerializedName
 
 class MainActivity : AppCompatActivity() {
 
-    private var formulario: Form? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario)
 
-        val formulario = getFormulario() // Este método obtiene un objeto Formulario
-
-        /*
-        val layoutPreguntas = findViewById<LinearLayout>(R.id.layout_preguntas)
-        val radioGroups = mutableListOf<RadioGroup>()
-        for (pregunta in formulario.preguntas) {
-            val textoPregunta = TextView(this)
-            textoPregunta.text = pregunta.texto
-            layoutPreguntas.addView(textoPregunta)
-
-            val radioGroup = RadioGroup(this)
-            radioGroups.add(radioGroup)
-            for (opcion in pregunta.opciones) {
-                val radioButton = RadioButton(this)
-                radioButton.text = opcion
-                radioGroup.addView(radioButton)
-            }
-            layoutPreguntas.addView(radioGroup)
-        }
-
-        val botonValidar = Button(this)
-        botonValidar.text = "Validar"
-        botonValidar.setOnClickListener {
-            var correctas = 0
-            for ((index, radioGroup) in radioGroups.withIndex()) {
-                val idSeleccionado = radioGroup.checkedRadioButtonId
-                if (idSeleccionado != -1) {
-                    val radioButtonSeleccionado = radioGroup.findViewById<RadioButton>(idSeleccionado)
-                    val opcionSeleccionada = radioGroup.indexOfChild(radioButtonSeleccionado)
-                    if (opcionSeleccionada == formulario.preguntas[index].respuestaCorrecta) {
-                        correctas++
-                        radioButtonSeleccionado.setTextColor(Color.GREEN)
-                    } else {
-                        radioButtonSeleccionado.setTextColor(Color.RED)
-                    }
-                }
-            }
-            val mensaje = "Respuestas correctas: $correctas/${formulario.preguntas.size}"
-            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
-        }
-        layoutPreguntas.addView(botonValidar)
-
-         */
+        getFormulario() // Este método obtiene un objeto Formulario
     }
 
     fun jsonToForm(json: String): Form {
@@ -77,24 +32,56 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getFormulario() {
-        /*
-        val preguntas = mutableListOf(
-            Question(1, "¿Cuál es tu color favorito?","random", listOf("Rojo", "Azul", "Verde", "Amarillo"), 1),
-            Question(2, "¿Cuál es tu comida favorita?","random",  listOf("Pizza", "Hamburguesa", "Tacos", "Sushi"), 0),
-            Question(3, "¿Cuál es tu deporte favorito?","random",  listOf("Fútbol", "Béisbol", "Baloncesto", "Tenis"), 2)
-        )
-        return Form(1, "random", "Formulario de ejemplo", preguntas)
-
-         */
         val url = "https://opoblueblood.000webhostapp.com/"
         getJsonFromUrlAsync(url, object : JsonCallback {
             override fun onJsonReceived(json: String) {
                 println(json)
                 // Aquí puedes procesar el JSON recibido
-                val form = jsonToForm(json)
-                val pregunta = form.preguntas[0].texto
+                val formulario = jsonToForm(json)
+                val pregunta = formulario.preguntas[0].texto
                 println(pregunta)
 
+                runOnUiThread {
+                    setContentView(R.layout.activity_formulario)
+                    val layoutPreguntas = findViewById<LinearLayout>(R.id.layout_preguntas)
+                    val radioGroups = mutableListOf<RadioGroup>()
+                    for (pregunta in formulario.preguntas) {
+                        val textoPregunta = TextView(this@MainActivity)
+                        textoPregunta.text = pregunta.texto
+                        layoutPreguntas.addView(textoPregunta)
+
+                        val radioGroup = RadioGroup(this@MainActivity)
+                        radioGroups.add(radioGroup)
+                        for (opcion in pregunta.opciones) {
+                            val radioButton = RadioButton(this@MainActivity)
+                            radioButton.text = opcion
+                            radioGroup.addView(radioButton)
+                        }
+                        layoutPreguntas.addView(radioGroup)
+                    }
+
+                    val botonValidar = Button(this@MainActivity)
+                    botonValidar.text = "Validar"
+                    botonValidar.setOnClickListener {
+                        var correctas = 0
+                        for ((index, radioGroup) in radioGroups.withIndex()) {
+                            val idSeleccionado = radioGroup.checkedRadioButtonId
+                            if (idSeleccionado != -1) {
+                                val radioButtonSeleccionado = radioGroup.findViewById<RadioButton>(idSeleccionado)
+                                val opcionSeleccionada = radioGroup.indexOfChild(radioButtonSeleccionado)
+                                if (opcionSeleccionada == formulario!!.preguntas[index].respuestaCorrecta) {
+                                    correctas++
+                                    radioButtonSeleccionado.setTextColor(Color.GREEN)
+                                } else {
+                                    radioButtonSeleccionado.setTextColor(Color.RED)
+                                }
+                            }
+                        }
+                        val mensaje = "Respuestas correctas: $correctas/${formulario!!.preguntas.size}"
+                        Toast.makeText(this@MainActivity, mensaje, Toast.LENGTH_SHORT).show()
+                    }
+                    layoutPreguntas.addView(botonValidar)
+                }
             }
 
             override fun onError(exception: Exception) {
@@ -103,7 +90,6 @@ class MainActivity : AppCompatActivity() {
         })
 
     }
-
 
     interface JsonCallback {
         fun onJsonReceived(json: String)
@@ -126,8 +112,4 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
-
-
-
-
 }
